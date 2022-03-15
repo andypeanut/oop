f57653d798d128b5847f569e2ce0cfee2316dc68
kimjunseo <a@.com>
2022-03-15 23:06:35 +0900

    0315

---
+++ 0315/BoxingUnboxingApp/BoxingUnboxingApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class BoxingUnboxingApp
+{
+    public static void Main()
+    {
+        int foo = 526;
+        object bar = foo; 
+        Console.WriteLine(bar);
+        try
+        {
+            double d = (short)bar;
+
+            Console.WriteLine(d);
+        }
+        catch (InvalidCastException e)
+        {
+            Console.WriteLine(e + "Error");
+        }
+    }
+}
---
+++ 0315/LeapYear/LeapYear/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("년도 입력: ");
+            int year = int.Parse(Console.ReadLine());
+
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            {
+                Console.WriteLine("윤년");
+            }
+            else
+            {
+                Console.WriteLine("평년");
+            }
+        }
+    }
+}
